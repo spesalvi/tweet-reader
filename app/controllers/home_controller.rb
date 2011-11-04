edcf0947c2a3da299b2602ec95f39fe137d4e416
Robert George <robert@poovelil.org>
2011-11-04 21:29:21 +0530

    Intial commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,35 @@
+class HomeController < ApplicationController
+  def index
+    configure
+    @news = get_latest_news
+  end
+
+  private
+
+  def configure
+    Twitter.configure do |config|
+      config.consumer_key = 'SMDBMhkytZHlgyIDa5OivA'
+      config.consumer_secret = 'FOKArGwrPG5TSxB39a0amUS8Qo7ua2GWLktyK11tMF4'
+      config.oauth_token = '208360590-9Q5M268iPghx1G07iwjEu0tKk4fhl9BUHtRIuCqY'
+      config.oauth_token_secret = '1qH2l6PzBNKYxsLsilr8OjnDC0xDHNtcNZ8UpPbxA'
+    end
+  end
+
+  def get_latest_news
+    news = []
+    timeline = Twitter.home_timeline(:include_entities => true)
+    timeline.each do |time_line|
+      next if time_line.entities.urls.empty?
+      a_news = { :screen_name => time_line.user.screen_name,
+        :user_profile_image_url => time_line.user.profile_image_url,
+        :tweet_text => time_line.text,
+        :url => []}
+      time_line.entities.urls.each do |url|
+        a_news[:url].push (! url.expanded_url.nil? ? url.expanded_url : url.url)
+      end
+      news.push a_news
+    end 
+    news
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterReader::Application.config.secret_token = 'f1113bf6b4c97ecd29d40405c7e4a0c525201d84831169bbf443f3b0a9bf3de6a533441d43b8090f3c9d08530ac32c3b3932a553d11afd108bf7f4731f0a2bf3'
